bd1c7d3a66f8d9c51457cdff5544cdefa01cb264
Bernard Diamante <bernardiamante@gmail.com>
2022-04-11 17:01:16 +0800

    Dashboard, item status updates based on count, item list +/check, project site filter
    Co-authored-by: Andrew Altuna <andrewaltuna@users.noreply.github.com>
    Co-authored-by: Leonard Narvaez <jean.narvaez@obf.ateneo.edu>
    Co-authored-by: AlexiJohn <AlexiJohn@users.noreply.github.com>
    Co-authored-by: p-prm <p-prm@users.noreply.github.com>
    Co-authored-by: KarloAlonzoGuiang <KarloAlonzoGuiang@users.noreply.github.com>
    Co-authored-by: MiguelGarciaIII <MiguelGarciaIII@users.noreply.github.com>

--- imrs/inventory/signals.py
+++ imrs/inventory/signals.py
@@ -1,26 +1,34 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from project_site.models import Inventory
 from django.dispatch import receiver
 
 @receiver(pre_save, sender=Inventory)
-def calculate_item_status(sender, instance, **kwargs):
+def calculate_item_status_pre_save(sender, instance, **kwargs):
+    mult = 0
     # If value has not been updated or if item does not exist:
     if instance.siteItemCount == Inventory.objects.filter(pk=instance.pk).values_list('siteItemCount', flat=True):
         pass
     else:
-        if instance.siteItemTurnover == 'f':
+        if instance.siteItemTurnover == 'F':
             mult = 1.5
-        elif instance.siteItemTurnover == 'n':
+        elif instance.siteItemTurnover == 'N':
             mult = 1.25
-        elif instance.siteItemTurnover == 's':
+        elif instance.siteItemTurnover == 'S':
             mult = 1
-        instance.siteItemMinThreshold *= mult
+        # instance.siteItemMinThreshold *= mult
             
-        if instance.siteItemCount >= instance.siteItemMinThreshold:
+        if instance.siteItemCount >= (instance.siteItemMinThreshold * mult):
             instance.siteItemStatus = 1
-        elif (instance.siteItemCount < instance.siteItemMinThreshold) and (instance.siteItemCount > 0):
+        elif (instance.siteItemCount < (instance.siteItemMinThreshold * mult)) and (instance.siteItemCount > 0):
             instance.siteItemStatus = 2
         else:
             instance.siteItemStatus = 3
-        return instance.siteItemStatus
+        print("STATUS UPDATED")
 
+@receiver(post_save, sender=Inventory)
+def calculate_item_status_post_save(sender, created, instance, **kwargs):
+    if created:
+        print("Status is", instance.siteItemStatus)
+        instance.save()
+    else:
+        print(instance.siteItemStatus, "was just saved")
