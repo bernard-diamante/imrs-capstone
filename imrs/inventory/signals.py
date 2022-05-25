e15ffe14dbad25c93e85fa7475723e2c262aef28
Bernard Diamante <bernardiamante@gmail.com>
2022-05-25 15:01:38 +0800

    Automate quantity changes on transfers
    Co-authored-by: Leonard Narvaez <jean.narvaez@obf.ateneo.edu>
    Co-authored-by: KarloAlonzoGuiang <KarloAlonzoGuiang@users.noreply.github.com>
    Co-authored-by: p-prm <p-prm@users.noreply.github.com>
    Co-authored-by: Andrew Altuna <andrewaltuna@users.noreply.github.com>

--- imrs/inventory/signals.py
+++ imrs/inventory/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import pre_save, post_save
 from project_site.models import Inventory
+# from transfer.models import MaterialTransfer
 from django.dispatch import receiver
 import datetime
 
@@ -10,23 +11,25 @@ def calculate_item_status_pre_save(sender, instance, **kwargs):
         pass
     else:
         if instance.siteItemTurnover == 'F':
-            mult = 1.5
-        elif instance.siteItemTurnover == 'N':
-            mult = 1.25
-        elif instance.siteItemTurnover == 'S':
             mult = 1
-        # instance.siteItemMinThreshold *= mult
+        elif instance.siteItemTurnover == 'N':
+            mult = 0.8
+        elif instance.siteItemTurnover == 'S':
+            mult = 0.6
 
+        # Change status based on item count and turnover
         if instance.siteItemCount == 0: # Added 4/29
             instance.siteItemStatus = 4
         elif instance.siteItemCount >= (instance.siteItemMinThreshold * mult):
-            instance.siteItemStatus = 1
-        elif (instance.siteItemCount < (instance.siteItemMinThreshold * mult)) and (instance.siteItemCount > 0):
-            instance.siteItemStatus = 2
-        else:
-            instance.siteItemStatus = 3
+            instance.siteItemStatus = 0
 
-        # Where is status 0?
+        elif instance.siteItemCount >= (instance.siteItemMinThreshold * mult * 0.75):
+            instance.siteItemStatus = 1
+        elif instance.siteItemCount >= (instance.siteItemMinThreshold * mult * 0.50):
+            instance.siteItemStatus = 2
+        elif instance.siteItemCount >= (instance.siteItemMinThreshold * mult * 0.25):
+            instance.siteItemStatus = 3
+    
 
 @receiver(post_save, sender=Inventory)
 def auto_now_inventory_modified(sender, instance, **kwargs):
